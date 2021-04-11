dcccfbd0ac7969b06e634008d5e1f2cfbbf9caae
sushant <sushant128815@gmail.com>
2021-04-11 13:41:05 +0530

    Patch

--- Host.py
+++ Host.py
@@ -1,10 +1,12 @@
 import socket
 import multiprocessing
+import parse
 
 clientHost = ("0.0.0.0", 8000) #Host At 0.0.0.0 To Allow Remote Connections. 8000 Is Default For Clients.
 clientSocket = socket.socket(socket.AF_INET) #Create Socket For Clients
 clientSocket.bind(clientHost) #Attach Socket For Clients
 clientSocket.listen(5) #Listen On Client Socket
+clientSocket.setsockopt(socket.SOL_SOCKET, socket.SO_REUSEADDR, 1)
 
 minerHost = ("0.0.0.0", 1600) #Host At 0.0.0.0 To Allow Remote Connections. 1600 Is Default For Miners.
 minerSocket = socket.socket(socket.AF_INET) #Create Socket For Miners
@@ -12,7 +14,14 @@ minerSocket.bind(minerHost) #Attach Socket For Miners
 minerSocket.listen(5) #Listen On Miner Socket
 
 def handleClient(sock):
-    pass
+    conn, addr = sock
+    req = conn.recv(160).decode()
+    print(req[:4])
+    if req[:4].lower() == "send":
+        target, value = parse.parse("send {} {}", req.lower())
+        print(f"send request to {target} worth {value}")
+
+
 
 def handleMiner(sock):
     pass
